6438db277577617e5804a73e354021cd65438942
Julia_Vladomira <juliavladomira@gmail.com>
2023-11-06 14:25:11 +0100

    remove some pages

--- src/helpers/routesArray.tsx
+++ src/helpers/routesArray.tsx
@@ -3,7 +3,6 @@ import { BookPage } from "../pages/BookPage";
 import { BooksPage } from "../pages/BooksPage";
 
 import { MyPage } from "../pages/MyPage";
-import { NotesPage } from "../pages/NotesPage";
 import { NotfoundPage } from "../pages/NotfoundPage";
 
 interface Routes {
@@ -16,10 +15,6 @@ export const routes: Routes[] = [
    { path: "/:id", RequireAuth: false, element: <BookPage /> },
    { path: "/auth", RequireAuth: false, element: <AuthPage /> },
    { path: "/user/:userId", RequireAuth: true, element: <MyPage /> },
-   {
-      path: "/user/:userId/my-notes",
-      RequireAuth: true,
-      element: <NotesPage />,
-   },
+
    { path: "/*", RequireAuth: false, element: <NotfoundPage /> },
 ];
--- src/pages/NotesPage.tsx
+++
@@ -1,9 +0,0 @@
-export const NotesPage = () => {
-   return (
-      <>
-         <h1>Notespage</h1>
-
-         <button type="button">Log out</button>
-      </>
-   );
-};
